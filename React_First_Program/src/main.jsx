72be4242438dba24ac8e16c12e70f407f63f4f99
artem <artsiomharminovich05@gmail.com>
2024-12-06 16:25:39 +0200

    Fiche variable "addressBlog"

--- React_First_Program/src/main.jsx
+++ React_First_Program/src/main.jsx
@@ -4,20 +4,19 @@ import './index.css'
 import React from "react";
 import App from './App.jsx'
 
-
-const address = {
-    city: 'Vilnius',
-    street: 'Sausion',
-    house: '78',
-    apart: '12',
-
-}
+const itemsConfig = [
+    {name: 'Apple', price: "1300", model: 'Iphone 16pro max', id: '12'},
+    {name: 'Samsung', price: "2500", model: 's24 ultra', id: '11'},
+]
 
 
-
-const addressBlog = <div>
-    {address.city ? <div>Welcome your city: {address.city}</div> : <div>Provide your address</div>}
-    {address.city && <div>Welcome your street: {address.street}</div>}
+const listOfItems = <div>
+    {itemsConfig.map((items) => <div className='wrapper' key={items.id}>
+        <div>{items.name}</div>
+        <div>{items.model}</div>
+        <div>{items.price}</div>
+    </div>)}
 </div>
 
-createRoot(document.getElementById('root')).render(addressBlog)
+
+createRoot(document.getElementById('root')).render(listOfItems)
